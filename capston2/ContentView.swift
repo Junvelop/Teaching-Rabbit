9ac7b9030d61f8ececd313da5322a99f4f249af9
parkhyunjun <wprzl479@naver.com>
2024-05-04 15:55:33 +0900

    Initial Commit

---
+++ capston2/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  capston2
+//
+//  Created by 박현준 on 5/4/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ capston2/capston2App.swift
@@ -0,0 +1,17 @@
+//
+//  capston2App.swift
+//  capston2
+//
+//  Created by 박현준 on 5/4/24.
+//
+
+import SwiftUI
+
+@main
+struct capston2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
